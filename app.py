9b7bf8714663200fe5afd8ce8b45e5b90de0bb47
Maxwell DeMaio <maxdman321@gmail.com>
2021-01-01 19:53:02 -0500

    Added update HTTP method to REST api

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ class VideoModel(db.Model):
         return f"Video (name={name}, views={views}, likes={likes}"
 
 
-# Request parser object and arguments for video object
+# Request parser object and arguments for video object (put)
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, 
     help='Please pass a name for the video', required=True)
@@ -27,6 +27,16 @@ video_put_args.add_argument("views", type=str,
     help='Please pass views of the video', required=True)
 video_put_args.add_argument("likes", type=str,
     help='Please pass likes on the video', required=True)
+
+# Request parser object and arguments for video object (patch)
+video_patch_args = reqparse.RequestParser()
+video_patch_args.add_argument("name", type=str,
+    help='Please pass a name for the video')
+video_patch_args.add_argument("views", type=str,
+    help='Please pass views of the video')
+video_patch_args.add_argument("likes", type=str,
+    help='Please pass likes on the video')
+
 
 # Define how DB object should be serialized
 resource_fields = {
@@ -53,13 +63,30 @@ class Video(Resource):
         result = VideoModel.query.filter_by(id=video_id).first()
         if result:
             abort(409, message="Video ID already exists in the database")
-
         args = video_put_args.parse_args()
         video = VideoModel(id=video_id, name=args['name'],
             views=args['views'], likes=args['likes'])
         db.session.add(video)
         db.session.commit()
         return video, 201
+
+    # Update video in DB
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        # Check if not in DB
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video ID does not exist in the database")
+        args = video_patch_args.parse_args()
+        # Update DB row with arguments
+        if 'name' in args and args['name'] != None:
+            result.name = args['name']
+        if 'likes' in args and args['likes'] != None:
+            result.likes = args['likes']
+        if 'views' in args and args['views'] != None:
+            result.views = args['views']
+        db.session.commit()
+        return result
 
     # Delete video
     def delete(self, video_id):
--- test.py
+++ test.py
@@ -8,12 +8,19 @@ data = [
     {"name": 'Go tutorial', 'views': 2000, "likes": 58},
 ]
 
+# Put
 for i in range(len(data)):
     response = requests.put(BASE + "video/" + str(i), data[i])
     print(response.json())
 
+# Delete
 # response = requests.delete(BASE + "video/2")
 # print(response)
 
+# Get
 response = requests.get(BASE + "video/2")
+print(response.json())
+
+# Update
+response = requests.patch(BASE + "video/2", {'views': 22, 'likes': 9678})
 print(response.json())
